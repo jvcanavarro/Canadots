8c6fa9843b2e8aa4027f8293a94206ed9e39b4f6
jvcanavarro <jvcanavarro@gmail.com>
2019-09-02 22:38:51 -0300

    Initalizing my Personal Instalation Script

---
+++ complete-install.sh
@@ -0,0 +1,30 @@
+# I asume you are on a Arch based system. For Ubuntu / Mint distros some modifications in this script are necessary.
+
+sudo su
+
+# i) Update Packages
+
+pacman -Syu
+
+# ii) Dotfiles and Default Scripts
+
+# Git clone Personal Dots
+# Git clone Luke LARBS
+
+# iii) Personal Preferred Applications
+
+# Awesome-WM
+# Ranger
+# Gotop
+# ...
+
+# iv) University Necessities
+
+# Python Packages
+# Any necessary software for classes
+
+# v) Random Stuff
+
+# qutebrower
+# neofetch
+# ...
