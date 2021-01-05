8ef49c0c22de19325e5349a99b1c2e05a1e9af5e
João Canavarro <jvcanavarro@gmail.com>
2021-01-05 12:59:01 -0300

    Update fish prompt with wakatime process

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -85,4 +85,14 @@ function fish_prompt
     end
 
     echo -n -s $arrow ' '$cwd $repo_info $normal ' '
+
+    set -l project
+
+    if echo (pwd) | grep -qEi "^/Users/$USER/Sites/"
+        set  project (echo (pwd) | sed "s#^/Users/$USER/Sites/\\([^/]*\\).*#\\1#")
+    else
+        set  project "Terminal"
+    end
+
+    wakatime --write --plugin "fish-wakatime/0.0.1" --entity-type app --project "$project" --entity (echo $history[1] | cut -d ' ' -f1) 2>&1 > /dev/null&; disown
 end
