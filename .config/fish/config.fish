cf0a93e48a66533e6b6a4f5730ed89f94bfdf953
João Canavarro <jvcanavarro@gmail.com>
2021-02-12 21:42:51 -0300

    Update fish shell with wakatime and kitty

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,7 @@
 #     set BASE16_SHELL "$HOME/.config/base16-shell/"
 #     source "$BASE16_SHELL/profile_helper.fish"
 # end
+# set fish_greeting
 
 alias py="python3.7"
 alias icat="kitty +kitten icat"
