0082e2559df3f6fa147afe3c5c9e669104db7c71
João Canavarro! <jvcanavarro@gmail.com>
2021-03-22 20:27:36 -0300

    Remove unecessary configs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,3 @@
-# if status --is-interactive
-#     set BASE16_SHELL "$HOME/.config/base16-shell/"
-#     source "$BASE16_SHELL/profile_helper.fish"
-# end
-# set fish_greeting
-
 alias py="python3.7"
 alias icat="kitty +kitten icat"
 alias vim="nvim"
