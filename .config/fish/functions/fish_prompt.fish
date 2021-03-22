c6978e784a1b1c3cc922ef35a83af14cda271a8b
João Canavarro! <jvcanavarro@gmail.com>
2021-03-22 15:59:48 -0300

    Add neofetch config

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -93,6 +93,4 @@ function fish_prompt
     else
         set  project "Terminal"
     end
-
-    wakatime --write --plugin "fish-wakatime/0.0.1" --entity-type app --project "$project" --entity (echo $history[1] | cut -d ' ' -f1) 2>&1 > /dev/null&
 end
