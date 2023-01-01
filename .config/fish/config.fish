2851df35bcd0572c477587009492742adcf8244f
Joao Canavarro <jvcanavarro@gmail.com>
2023-01-01 11:00:46 -0300

    add xorg conf and nvidia conf, removed from .xprofile

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,9 @@
 alias py="python3.7"
 alias icat="kitty +kitten icat"
 alias vim="nvim"
+alias ls="exa --icons"
+alias la="ls -lab --no-permissions --no-user"
+alias yarn='yarn --network-timeout 100000'
+
+starship init fish | source
+motivate 3 7 8 1 2 8
